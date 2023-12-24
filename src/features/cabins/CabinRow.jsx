899e1be69291f5a213164f42b22f70006df3ff24
Rasel Hossen <raselhossen052@gmail.com>
2023-12-24 22:54:11 +0600

    validate_form_input

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
-import formatCurrency from '../../utils/helpers'
+import formatCurrency from '../../utils/helpers';
+import { deleteCabin } from '../../services/apiCabins';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -39,15 +42,40 @@ const Discount = styled.div`
   color: var(--color-green-700);
 `;
 const CabinRow = ({ cabin }) => {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+  const queryClient = useQueryClient();
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success('Cabin successfully deleted');
+      queryClient.invalidateQueries({
+        queryKey: ['cabin'],
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
   return (
     <TableRow role='row'>
       <Img src={image} />
       <Cabin>{name}</Cabin>
-      <div>Fits up tp {maxCapacity}</div>
+      <div>Fits up to {maxCapacity}</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button
+        onClick={() => mutate(cabinId)}
+        disabled={isDeleting}
+      >
+        Delete
+      </button>
     </TableRow>
   );
 };
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -32,7 +32,7 @@ const CabinTable = () => {
     data: cabins,
     error,
   } = useQuery({
-    queryKey: ['cabin'],
+    queryKey: ['cabins'],
     queryFn: getCabins,
   });
   if (isLoading) return <Spinner />;
