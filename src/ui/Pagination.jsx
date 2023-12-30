ee3b456a48ebbedc7d0fd06ecc20bdd53cf10583
Rasel Hossen <raselhossen052@gmail.com>
2023-12-30 13:04:55 +0600

    prefetching_data_with_react_query

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,5 +1,7 @@
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { PAGE_SIZE } from '../utils/constants';
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -56,17 +58,45 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-const Pagination = () => {
+
+const Pagination = ({ count }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = !searchParams.get('page')
+    ? 1
+    : Number(searchParams.get('page'));
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1;
+    searchParams.set('page', next);
+    setSearchParams(searchParams);
+  }
+  function prevPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1;
+    searchParams.set('page', prev);
+    setSearchParams(searchParams);
+  }
+  if (pageCount <= 1) return null;
   return (
     <StyledPagination>
       <P>
-        Showing <span>1</span> to <span>10</span> of <span>23</span> results
+        Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to
+        <span>
+          {' '}
+          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
+        </span>{' '}
+        of <span>{count}</span> results
       </P>
       <Buttons>
-        <PaginationButton>
+        <PaginationButton
+          onClick={prevPage}
+          disabled={currentPage === 1}
+        >
           <HiChevronLeft /> <span>Previous</span>
         </PaginationButton>
-        <PaginationButton>
+        <PaginationButton
+          onClick={nextPage}
+          disabled={currentPage === pageCount}
+        >
           <HiChevronRight /> <span>Next</span>
         </PaginationButton>
       </Buttons>
